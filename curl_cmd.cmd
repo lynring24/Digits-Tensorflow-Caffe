2802a551a46eed59e6b585544899a4e1e47eec93
etri <etri@etri.com>
2018-07-09 10:28:14 +0900

    for presentation

--- curl_cmd.cmd
+++ curl_cmd.cmd
@@ -12,8 +12,12 @@ C:\Digits\digits\jobs\20180706-111701-2e5b
 curl localhost:5000/models/images/classification.json -b digits.cookie -XPOST -F method=standard -F standard_networks=lenet -F train_epochs=30 -F framework=caffe -F model_name=lenet_base -F dataset=20180706-111701-2e5b
 job_id = '20180706-115037-2f35'
 curl localhost:5000/models/20180706-115037-2f35.json
+
 # classificaion
 curl localhost:5000/models/images/classification/classify_many.json -XPOST -F job_id=20180706-115037-2f35 -F image_list=@D:\MINST\Extraction\extracted.txt > predictions.txt
+
+# model description
+curl localhost:5000/datasets/20180706-170647-78f1.json
 
 
 # create model2  : difference in epoches x3
@@ -23,17 +27,15 @@ curl localhost:5000/models/images/classification/classify_many.json -XPOST -F jo
 
 # create model3  : difference in epoches x6
 curl localhost:5000/models/images/classification.json -b digits.cookie -XPOST -F method=standard -F standard_networks=lenet -F train_epochs=200 -F framework=caffe -F model_name=lenet_200 -F dataset=20180706-111701-2e5b
-job_id :20180705-154809-8bc1
-curl localhost:5000/models/images/classification/classify_many.json -XPOST -F job_id=20180705-154809-8bc1 -F image_list=@D:\MINST\Extraction\extracted.txt > D:\Crawler\lenet200.json
-
-# create model4  : difference in solver type
-curl localhost:5000/models/images/classification.json -b digits.cookie -XPOST -F method=standard -F standard_networks=alexnet -F train_epochs=30 -F framework=caffe -F model_name=alexnet -F dataset=20180706-111701-2e5b
 job_id :
+curl localhost:5000/models/images/classification/classify_many.json -XPOST -F job_id=20180705-154809-8bc1 -F image_list=@D:\MINST\Extraction\extracted.txt > D:\Crawler\lenet_200.json
 
 # create model3  : difference in epoches 1
 curl localhost:5000/models/images/classification.json -b digits.cookie -XPOST -F method=standard -F standard_networks=lenet -F train_epochs=1 -F framework=caffe -F model_name=lenet_1 -F dataset=20180706-111701-2e5b
 job_id : 20180706-170647-78f1
 curl localhost:5000/models/images/classification/classify_many.json -XPOST -F job_id=20180706-170647-78f1 -F image_list=@D:\MINST\Extraction\extracted.txt > D:\Crawler\lenet_1.txt
 
+# create model with adadelta  : difference in solver type
+curl localhost:5000/models/images/classification.json -b digits.cookie -XPOST -F method=standard -F standard_networks=lenet -F train_epochs=1 -F framework=caffe -F model_name=lenet_1 -F dataset=20180706-111701-2e5b
 20180706-173832-c600
 curl localhost:5000/models/images/classification/classify_many.json -XPOST -F job_id=20180706-173832-c600 -F image_list=@D:\MINST\Extraction\extracted.txt > D:\Crawler\lenet_1_adadelta.txt
